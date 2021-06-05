089ab117297d4c25655aa82cb92607fdc308d632
Mateusz Michalski <michalskimateusz@outlook.com>
2021-06-05 13:40:04 +0200

    Implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,15 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/ResultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -47,17 +48,27 @@ const controlSearchResults = async function () {
     //2 load search results
     await model.loadSearchResults(query);
 
-    //render results
-    resultsView.render(model.state.search.results)
+    //3 render results
+    resultsView.render(model.getSearchResultsPage());
+
+    //4 render initial pagination buttons
+    paginationView.render(model.state.search)
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  //1 render new results
+   resultsView.render(model.getSearchResultsPage(goToPage));
 
+  //2 render new pagination buttons
+  paginationView.render(model.state.search)
+}
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 }
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
 import { async } from "regenerator-runtime";
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -47,3 +49,11 @@ export const loadSearchResults = async function (query) {
     throw err;
   }
 };
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;//0;
+  const end = page * state.search.resultsPerPage; //9;
+
+  return state.search.results.slice(start, end)
+}
